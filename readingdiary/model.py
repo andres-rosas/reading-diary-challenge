c5db480e3916587174a71632bf40299287771a63
Andres <andres.erc2022@gmail.com>
2026-03-09 17:18:53 -0500

    Segunda parte! :)

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from typing import List
+
 
 class Note:
-    def __init(self, text: str, page: int, date: datetime  ):
-        self.text = text
-        self.page = page
-        self.date = date
+    def __init(self, text: str, page: int, date: datetime):
+        self.text :str = text
+        self.page :int = page
+        self.date :datetime = date
 
     def __str__(self):
         return f"{self.text} {self.page} {self.date}"
@@ -16,8 +18,18 @@ class Book:
     UNRATED = -1
 
     def __init__(self, isbn : str, title: str, author: str, pages: int):
-        self.isbn = isbn
-        self.title = title
-        self.author = author
-        self.pages = pages
+        self.isbn :str = isbn
+        self.title :str = title
+        self.author :str = author
+        self.pages :int = pages
+        self.rating :int = Book.UNRATED
+        self.notes :List[Note] = []
+
+    def add_note(self, text : str, page : int, date: datetime)-> bool:
+
+
+
+
+
+
 
