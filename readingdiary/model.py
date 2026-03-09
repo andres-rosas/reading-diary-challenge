c1ad36ab0629458a7579c44996db5a1aaba4d836
Andres <andres.erc2022@gmail.com>
2026-03-09 17:06:42 -0500

    Primera parte lista

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,23 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Note:
+    def __init(self, text: str, page: int, date: datetime  ):
+        self.text = text
+        self.page = page
+        self.date = date
+
+    def __str__(self):
+        return f"{self.text} {self.page} {self.date}"
+
+class Book:
+    EXCELLENT = 3
+    GOOD = 2
+    BAD = 1
+    UNRATED = -1
+
+    def __init__(self, isbn : str, title: str, author: str, pages: int):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.pages = pages
+
